00b4f906128e79a4bf1992b2720e4b81dccbb05d
Amer Mall <amer@icloud.com>
2021-11-28 21:38:48 -0500

    Assignment completed

---
+++ index.js
@@ -0,0 +1,11 @@
+// has a function expression called divide
+const divide = function() {
+    return (2000 / 100);
+}
+
+// has an arrow function called square
+const square = (num1) => num1 * num1;
+
+// 1) has an arrow function called add...
+// 2) add arrow function takes two parameters and adds them together
+const add = (number1, number2) => number1 + number2;
